fe103ec4df2dcd743ce5d8816458b2d6576d6903
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-10-05 16:54:43 +0900

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+	public static void main(String[] args){
+		int num = subtract(8, 5);
+		System.out.println(num);
+	}
+
+	public static int subtract(int a, int b){
+		return a - b;
+	}
+
+}
