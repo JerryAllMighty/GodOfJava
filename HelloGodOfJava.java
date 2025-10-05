df8dd53814b2d585f6ebdda21b91447f6a593730
KimYoonMin <dbsalszz@naver.com>
2025-10-05 15:40:39 +0900

    God Of Java Initial Compile file and source code file

---
+++ HelloGodOfJava.java
@@ -0,0 +1,8 @@
+public class HelloGodOfJava {
+	public static void main(String[] args){
+		System.out.println("Hello God Of Java");
+		System.out.print("Hello God Of Java22");
+		System.out.println("ABCD");
+	}
+
+}
